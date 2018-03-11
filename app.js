45f9a410d07bd93e88fff27a5958295b05f2796c
csergey8 <c.sergey@protonmail.com>
2018-03-11 09:13:55 +0200

    added localStorage

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-// Init Weather
-const weather = new Weather('Boston', 'MA');
-
 // Init UI
 const ui = new UI();
+
+// Inint Storage
+const storage = new Storage();
+const weatherLocation = storage.getLocationData();
+
+// Init Weather
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
+
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+
+    weather.changeLocation(city, state);
+    storage.setLocationData(city, state);
+    getWeather();
+
+    $('#locModal').modal('hide');
+});
 
 document.addEventListener('DOMContentLoaded', getWeather);
 
